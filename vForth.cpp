b51eff80f2f6f5d7a4d4e576a22159a003b4d95f
Dominic Pace <dompace181@gmail.com>
2019-05-11 15:28:22 -0500

    commandline stuff

--- vForth.cpp
+++ vForth.cpp
@@ -3,27 +3,47 @@
 
 
 #include <iostream>
+#include <fstream>
 #include "Forth.hpp"
 
 
-int main()
+int main(int argc, char** argv)
 {
     
-	Forth f(true);
-
-	if (true) {
+	if (argc > 1) {
+		ifstream source(argv[1]);
+		string inp;
+		vector<string> lines;
+		while (!source.eof()) {
+			std::getline(source, inp);
+			lines.push_back(inp);
+		}
+		Forth finterp(false);
+		for (auto line : lines) {
+			finterp.interp(line);
+		}
+		
 		
 
-		cout << "vForth-cpp by Dominic Pace\n";
-		cout << "Compiled on " << __DATE__ << " at " << __TIME__ << "\n";
-		while (true) {
-			cout << ">";
-			string inp;
-			std::getline(std::cin, inp);
-			f.interp(inp);
-			cout << endl << "ok" << endl;
-			
-			
+	}
+	else {
+
+		Forth f(true);
+
+		if (true) {
+
+
+			cout << "vForth-cpp by Dominic Pace\n";
+			cout << "Compiled on " << __DATE__ << " at " << __TIME__ << "\n";
+			while (true) {
+				cout << ">";
+				string inp;
+				std::getline(std::cin, inp);
+				f.interp(inp);
+				cout << endl << "ok" << endl;
+
+
+			}
 		}
 	}
 	return 0;
